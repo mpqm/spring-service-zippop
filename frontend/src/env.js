8f50f2c86e86eeb0045f58fa6d9428d03a499ce9
mpqm <okqkrwhdtjd@gmail.com>
2024-11-27 18:10:31 +0900

    [cicd] GithubAction 프론트엔드 배포 프로세스 TEST 10

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -1,4 +1,4 @@
-export const backend = "http://43.201.68.44:8080/api/v1";
+export const backend = "https://mpqm-zippop-backend.kro.kr:8080/api/api/v1";
 export const IAMPORT_UID = "imp05841528";
 export const IAMPORT_PG = "kakaopay.TC0ONETIME"
 export const IAMPORT_NAME = "ZIPPOP PAY"
