7f9bfb76e48a20de01194f731fdf167fd3d86d7e
mpqm <okqkrwhdtjd@gmail.com>
2024-11-28 01:04:45 +0900

    [fix] 이메일 리다이렉션 URL 수정

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -1,5 +1,5 @@
-// export const backend = "https://mpqm-zippop-backend.kro.kr/api/api/v1";
-export const backend = "http://localhost:8080/api/v1";
+export const backend = "https://mpqm-zippop-backend.kro.kr/api/api/v1";
+// export const backend = "http://localhost:8080/api/v1";
 export const IAMPORT_UID = "imp05841528";
 export const IAMPORT_PG = "kakaopay.TC0ONETIME"
 export const IAMPORT_NAME = "ZIPPOP PAY"
